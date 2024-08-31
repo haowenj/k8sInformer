583ce28cee036b8bc45232e7bb33416256ac3a31
wenju <15102151343@163.com>
2024-08-31 11:51:21 +0800

    修改readme，修改kubevirt引入方式

--- informerfactory/kubevirt/register_resource.go
+++ informerfactory/kubevirt/register_resource.go
@@ -2,36 +2,22 @@ package kubevirt
 
 import (
 	"fmt"
-	"os"
-
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/kubernetes/scheme"
-	"kubevirt.io/api/core"
 	virtv1 "kubevirt.io/api/core/v1"
 )
 
 var (
-	SchemeBuilder  runtime.SchemeBuilder
-	Scheme         *runtime.Scheme
-	Codecs         serializer.CodecFactory
-	ParameterCodec runtime.ParameterCodec
+	Scheme *runtime.Scheme
+	Codecs serializer.CodecFactory
 )
 
 func init() {
-	registerVersion := os.Getenv(virtv1.KubeVirtClientGoSchemeRegistrationVersionEnvVar)
-	if registerVersion != "" {
-		SchemeBuilder = runtime.NewSchemeBuilder(virtv1.AddKnownTypesGenerator([]schema.GroupVersion{{Group: core.GroupName, Version: registerVersion}}))
-	} else {
-		SchemeBuilder = runtime.NewSchemeBuilder(virtv1.AddKnownTypesGenerator(virtv1.GroupVersions))
-	}
 	Scheme = runtime.NewScheme()
-	AddToScheme := SchemeBuilder.AddToScheme
 	Codecs = serializer.NewCodecFactory(Scheme)
-	ParameterCodec = runtime.NewParameterCodec(Scheme)
-	utilruntime.Must(AddToScheme(Scheme))
-	utilruntime.Must(AddToScheme(scheme.Scheme))
+	utilruntime.Must(virtv1.AddToScheme(Scheme))
+	utilruntime.Must(virtv1.AddToScheme(scheme.Scheme))
 	fmt.Println("*********** kubevirt client go scheme ***********")
 }
